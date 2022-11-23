220b08c931c69a68bbe6d23a054daf077574ed0d
Sara-Abo-Shousha <77673106+Sara-Abo-Shousha@users.noreply.github.com>
2022-11-23 11:18:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const viz = new Viz();
+const body = document.querySelector("body");
+const trieEL = document.querySelector(".trie-graph .g-container");
+const dawgEL = document.querySelector(".dawg-graph .g-container");
+const trieTitle = document.querySelector(".trie-graph .title");
+const dawgTitle = document.querySelector(".dawg-graph .title");
+/**
+ * @type {HTMLInputElement}
+ */
+const userInput = document.querySelector(".inputContainer input[type=text]");
+const buildBtn = document.querySelector(".build-btn>button");
+(async () => {
+    buildGraphs(["sara", "lara", "yara"], {
+        trieEL,
+        dawgEL,
+        trieTitle,
+        dawgTitle,
+    });
+})();
+
+buildBtn.onclick = async() => {
+    const words = userInput.value
+        .split(",")
+        .map((w) => w.trim())
+        .filter((w) => w);
+    await buildGraphs(words, { trieEL, dawgEL, trieTitle, dawgTitle });
+    trieTitle.scrollIntoView()
+};
+
+userInput.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") buildBtn.onclick();
+});
